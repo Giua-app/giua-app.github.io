c6531ac2359fdd66b48f41a3fc6a568777c40dbc
HiemSword <hiemsword08@pm.me>
2021-10-04 21:21:18 +0200

    added loading icon

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,3 +1,43 @@
+/*
+* Custom scripts
+*/
+window.onload = checkAppVersion
+
+function checkAppVersion(){
+  $.getJSON("https://api.github.com/repos/giua-app/giua-app/releases",function(resultData){
+    var ver = resultData["0"]["tag_name"]
+    document.getElementById("ver-string").innerHTML = "Ultima versione: " + ver
+  });
+}
+
+function searchAndDownloadApp() {
+  var btn_download = document.getElementById("btn-download")
+  btn_download.setAttribute("onClick", "")
+  btn_download.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Download in corso...'
+  $.getJSON("https://api.github.com/repos/giua-app/giua-app/releases",function(resultData){
+    //var jsonP = JSON.stringify(resultData);
+    try {
+      var link = resultData["0"]["assets"]["0"]["browser_download_url"]
+    }
+    catch(e){
+      alert("C'è stato un errore nel download.\nErrore: " + e.message)
+      btn_download.innerHTML = "Errore :("
+      return;
+    }
+    //document.getElementById("btn-download").innerHTML = "Download completato"
+    window.open(link, "_top")
+    btn_download.innerHTML = "Download avviato"
+    setTimeout(function(){
+        btn_download.setAttribute("onClick", "searchAndDownloadApp();")
+        btn_download.innerHTML = "Scarica"
+    },2000);
+  });
+}
+
+
+
+
+
 /*!
 * Start Bootstrap - Scrolling Nav v5.0.3 (https://startbootstrap.com/template/scrolling-nav)
 * Copyright 2013-2021 Start Bootstrap
