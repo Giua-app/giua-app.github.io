3e5670428af81b7eed8f8ed09749b48389787f79
HiemSword <hiemsword08@pm.me>
2022-02-11 16:50:31 +0100

    Updated privacy policy

--- assets/scripts.js
+++ assets/scripts.js
@@ -11,11 +11,6 @@ window.onload = function(){
         checkAppVersion();
         return;
     }
-
-    if(document.title.startsWith("changelog")) {
-        buildChangelogForAPI();
-        return;
-    }
 }
 
 function buildChangelog(){
@@ -42,37 +37,6 @@ function buildChangelog(){
         div.innerHTML += "<br><br>"
     });
 }
-
-function buildChangelogForAPI(){
-    var div = document.getElementsByTagName("body")[0]
-
-    $.ajax({
-        type: "GET",
-        async: "false",
-        url: "https://api.github.com/repos/giua-app/giua-app/releases",
-        success: function(resultData){
-            var tag =  resultData["0"]["tag_name"]
-            var body =  resultData["0"]["body"]
-            var date = new Date(resultData["0"]["published_at"])
-
-            div.innerHTML = "<h1>" + tag + " <span style=\"font-size: 60%;\">(" + date.toLocaleDateString() + ")</span></h1>"
-            div.innerHTML += body
-
-            for (let i = 1; i < Object.keys(resultData).length; i++) {
-                var tag =  resultData["" + i]["tag_name"]
-                var body =  resultData["" + i]["body"]
-                var date = new Date(resultData["" + i]["published_at"])
-                if(body != ""){
-                    div.innerHTML += "<hr><h1>" + tag + " <span style=\"font-size: 60%;\">(" + date.toLocaleDateString() + ")</span></h1>"
-                    div.innerHTML += body
-                }
-            }
-
-            div.innerHTML += "<br><br>"
-        }
-    })
-}
-
 
 function checkAppVersion(){
   $.getJSON("https://api.github.com/repos/giua-app/giua-app/releases",function(resultData){
@@ -106,108 +70,6 @@ function searchAndDownloadApp() {
         },2000);
     });
 }
-
-
-/*
-* Animated canvas background
-* made by Tuomas Pöyry on https://codepen.io/Munkkeli/pen/PqWBdP
-*
-var Canvas = document.getElementById('canvas');
-var ctx = Canvas.getContext('2d');
-
-var resize = function() {
-    Canvas.width = Canvas.clientWidth;
-    Canvas.height = Canvas.clientHeight;
-};
-window.addEventListener('resize', resize);
-resize();
-
-var elements = [];
-var presets = {};
-
-presets.o = function (x, y, s, dx, dy) {
-    return {
-        x: x,
-        y: y,
-        r: 12 * s,
-        w: 5 * s,
-        dx: dx,
-        dy: dy,
-        draw: function(ctx, t) {
-            this.x += this.dx;
-            this.y += this.dy;
-            
-            ctx.beginPath();
-            ctx.arc(this.x + + Math.sin((50 + x + (t / 10)) / 100) * 3, this.y + + Math.sin((45 + x + (t / 10)) / 100) * 4, this.r, 0, 2 * Math.PI, false);
-            ctx.lineWidth = this.w;
-            ctx.strokeStyle = '#fff';
-            ctx.stroke();
-        }
-    }
-};
-
-presets.x = function (x, y, s, dx, dy, dr, r) {
-    r = r || 0;
-    return {
-        x: x,
-        y: y,
-        s: 20 * s,
-        w: 5 * s,
-        r: r,
-        dx: dx,
-        dy: dy,
-        dr: dr,
-        draw: function(ctx, t) {
-            this.x += this.dx;
-            this.y += this.dy;
-            this.r += this.dr;
-            
-            var _this = this;
-            var line = function(x, y, tx, ty, c, o) {
-                o = o || 0;
-                ctx.beginPath();
-                ctx.moveTo(-o + ((_this.s / 2) * x), o + ((_this.s / 2) * y));
-                ctx.lineTo(-o + ((_this.s / 2) * tx), o + ((_this.s / 2) * ty));
-                ctx.lineWidth = _this.w;
-                ctx.strokeStyle = c;
-                ctx.stroke();
-            };
-            
-            ctx.save();
-            
-            ctx.translate(this.x + Math.sin((x + (t / 10)) / 100) * 5, this.y + Math.sin((10 + x + (t / 10)) / 100) * 2);
-            ctx.rotate(this.r * Math.PI / 180);
-            
-            line(-1, -1, 1, 1, '#fff');
-            line(1, -1, -1, 1, '#fff');
-            
-            ctx.restore();
-        }
-    }
-};
-
-for(var x = 0; x < Canvas.width; x++) {
-    for(var y = 0; y < Canvas.height; y++) {
-        if(Math.round(Math.random() * 8000) == 1) {
-            var s = ((Math.random() * 5) + 1) / 10;
-            if(Math.round(Math.random()) == 1)
-                elements.push(presets.o(x, y, s, 0, 0));
-            else
-                elements.push(presets.x(x, y, s, 0, 0, ((Math.random() * 3) - 1) / 10, (Math.random() * 360)));
-        }
-    }
-}
-
-setInterval(function() {
-    ctx.clearRect(0, 0, Canvas.width, Canvas.height);
-
-    var time = new Date().getTime();
-    for (var e in elements)
-		elements[e].draw(ctx, time);
-}, 10);
-*/
-
-
 
 /*!
 * Start Bootstrap - Scrolling Nav v5.0.3 (https://startbootstrap.com/template/scrolling-nav)
---
+++ old_privacy.md
@@ -0,0 +1,59 @@
+---
+layout: page
+title: Privacy Policy
+description: La Privacy Policy di Giua App
+background: '/img/blue-bg.png'
+---
+
+# ⠀
+
+Ultimo aggiornamento: 19 Novembre 2021
+
+### Ciao!
+
+Innanzitutto, grazie per aver usato Giua App! Siamo lieti che tu voglia sapere di più su come trattiamo i tuoi dati, ed è per questo che abbiamo reso questo documento il più leggibile e comprendibile possibile, anche senza particolari conoscenze legislative o informatiche. Ricordiamo che *questa Privacy Policy riguarda **esclusivamente** l'app e non ricopre questo sito web*.
+
+## Che informazioni raccogliete?
+
+Nel caso un utente entri nel registro selezionando l’opzione “Ricorda credenziali”, l'app raccoglie e memorizza nella memoria interna del tuo telefono le credenziali (*nome utente* e *password* nel caso di account normale e *cookie di Google* nel caso di account con Google).
+
+L'unico caso in cui le credenziali vengono inviate in rete è quando l’app deve utilizzarle per entrare nel Registro Elettronico al posto tuo per velocizzare o automatizzare l'operazione. Questa funzione viene chiamata “Login Automatico” e viene effettuata all’avvio dell’app, nel caso la tua sessione scada durante l’esecuzione, o per controllare nuove notifiche.
+
+In particolari situazioni (vedi sezione *[In quali situazioni usate gli analytics nell'App?](#analytics)*) contattiamo il nostro provider di analytics ([Piratepx](https://www.piratepx.com/)). Questo contatto consiste in una richiesta HTTP GET e non include informazioni personali: serve soltanto a contare quante persone usano la nostra App.
+
+## Come usate le mie informazioni?
+
+Usiamo le tue informazioni solo per scopi legittimi. Ad esempio, nel caso delle informazioni di accesso o credenziali del Registro Elettronico, queste vengono usate localmente nel tuo telefono per effettuare l'accesso.
+
+## Le mie informazioni verrano condivise con qualcuno?
+
+No, non condividiamo le tue informazioni personali a nessun servizio esterno e neanche a noi stessi. Le informazioni di accesso e le tue preferenze non vengono trasmesse online e rimangono solo nella memoria interna del tuo telefono.
+
+## Con chi condividete le mie informazioni?
+
+Le uniche informazioni trasmesse e condivise al di fuori del tuo telefono sono gli analytics del nostro provider [Piratepx](https://www.piratepx.com/). Non sono definite "Informazioni personali" in quanto non ti identificano individualmente in nessun modo.
+
+## Per quanto tempo tenete le mie informazioni?
+
+Riguardo alle informazioni personali conservate nella memoria interna del telefono, vengono conservate fino a quando non viene disinstallata l'app. Invece, le informazioni inviate a [Piratepx](https://www.piratepx.com/) vengono conservate per 30 giorni.
+
+## In quali situazioni usate gli analytics nell'App?
+
+<div id="analytics"></div>
+
+Inviamo una richiesta HTTP GET al nostro provider di analytics [Piratepx](https://www.piratepx.com/) quando:
+
+- si avvia per la prima volta l'app (nuova installazione)
+- il login viene completato con successo
+- si esce dall'account, cioè si effettua il log out
+- viene visualizzato un "[Easter Egg](https://it.wikipedia.org/wiki/Easter_egg)"
+
+## Come posso contattarvi riguardo questa Privacy Policy?
+
+Puoi contattarci via email a *privacy@giua-app.slemail.me*
+
+<br>
+
+*In nessun caso gli sviluppatori di Giua App o i suoi fornitori saranno responsabili per qualsiasi danno (inclusi, senza limitazione, danni per perdita di dati o profitti, o dovuti all'interruzione dell'attività) derivanti dall'uso o dall'impossibilità di usare l'applicazione Giua App.*
+
+<br><br><br>
